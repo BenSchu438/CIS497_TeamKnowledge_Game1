6da70ad069fb7b9a4109a83a24f026875e8539d4
Jerod Lockhart <j.r.1327@gmail.com>
2021-03-07 21:36:10 -0600

    Saving music player

---
+++ MountainClimb/Assets/Scripts/RandomMusicPicker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicPicker : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
